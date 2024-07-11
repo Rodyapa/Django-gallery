1f5df39e147fd5348699cca5c600825b74978734
rodion <rpadakov@yandex.ru>
2024-07-11 17:02:02 +0800

    change navbar generation logic

--- backend/django_gallery/albums/utils.py
+++ backend/django_gallery/albums/utils.py
@@ -1,16 +1,16 @@
-from .models import Section
+from .models import Section, Album
 
 
 def find_all_child_sections_and_their_albums(section: Section) -> dict:
     daughter_sections = section.daughter_sections.all().filter(
         is_published=True)
-    if not daughter_sections:
-        return [album for album in section.albums.all().filter(
-            is_published=True
-        )]
-    else:
-        return {
-            daughter_section.title: find_all_child_sections_and_their_albums(
-                daughter_section)
-            for daughter_section in daughter_sections
-        }
+    section_albums = section.albums.all().filter(is_published=True)
+    return {'section_albums': [
+        album for album in section_albums
+        ],
+            'daughter_sections': {
+                                    daughter_section.title:
+                                    find_all_child_sections_and_their_albums
+                                    (daughter_section)
+                                for daughter_section in daughter_sections}
+            }
