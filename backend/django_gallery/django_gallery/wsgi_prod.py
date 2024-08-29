b427f1f07e12e0987ca0b8173881f676c603a844
rodion <rpadakov@yandex.ru>
2024-08-29 14:32:18 +0800

    debugging workflow

--- backend/django_gallery/django_gallery/wsgi_prod.py
+++ backend/django_gallery/django_gallery/wsgi_prod.py
@@ -8,9 +8,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_gallery.settings_prod')
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
