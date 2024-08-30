730fb181db8ba8b7a176a4d78a318854b8fe3b0b
rodion <rpadakov@yandex.ru>
2024-08-30 20:18:23 +0800

    fix css in admin photo instances

--- backend/django_gallery/admin_site/admin.py
+++ backend/django_gallery/admin_site/admin.py
@@ -75,7 +75,8 @@ class PhotoAdmin(admin.ModelAdmin):
 
     class Media:
         css = {
-            "all": ["admin/styles/sorting_zone.css", ],
+            "all": ["admin/styles/sorting_zone.css", 
+                    "admin/styles/admin_photos.css"],
         }
 
 
