a89d0f5a9285cec68826d19bc1b729bf53602f71
rodion <rpadakov@yandex.ru>
2024-08-30 18:37:08 +0800

    fix script that allows to browse photos with arrows keys

--- backend/django_gallery/albums/views.py
+++ backend/django_gallery/albums/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import date
 
 from albums.models import Album
@@ -27,34 +28,47 @@ class AlbumView(View):
         return 'albums/album.html'
 
     def get_photos(self, album, template_name):
-        photos = Photo.objects.filter(album=album, is_published=True)
+        photos = (Photo.objects
+                  .filter(album=album, is_published=True)
+                  .order_by('order'))
 
         if template_name == 'year_sorted':
-            photos_by_year = {}
+            photos_by_year = defaultdict(list)
+            frontend_dataset_counter = 0  # Counter for frontend dataset
+
             for photo in photos:
-                if photo.date:
-                    year_of_photo = photo.date.year
-                    if year_of_photo not in photos_by_year:
-                        photos_by_year[year_of_photo] = [photo, ]
-                    else:
-                        photos_by_year[year_of_photo].append(photo)
-                else:
-                    today_year = date.today().year
-                    if photos_by_year.get(str(today_year)):
-                        photos_by_year[str(today_year)].append(photo)
-                    else:
-                        photos_by_year[str(today_year)] = [photo, ]
+                photo.order_dataset = frontend_dataset_counter
+                frontend_dataset_counter += 1
+
+                year_of_photo = (
+                    photo.date.year if photo.date else date.today().year
+                )
+
+                photos_by_year[year_of_photo].append(photo)
+
             return dict(sorted(photos_by_year.items(), reverse=True))
         elif template_name == 'subdivided':
-            photos_by_subcategories = {}
-            for photo in photos:
-                photo_subcategory = photo.subcategory
-                if photo.subcategory:
-                    if photo_subcategory in photos_by_subcategories:
-                        photos_by_subcategories[photo_subcategory].append(
-                            photo
-                        )
-                    else:
-                        photos_by_subcategories[photo_subcategory] = [photo, ]
-            return photos_by_subcategories
+            unique_subcategories = {
+                photo.subcategory for photo in photos if (
+                    photo.subcategory is not None)
+            }
+
+            sorted_subcategories = sorted(
+                unique_subcategories, key=lambda x: x.order
+            )
+
+            photos_by_subcategory = {}
+            frontend_dataset_counter = 0  # For Frontend Dataset
+
+            for subcategory in sorted_subcategories:
+                photos_in_subcategory = subcategory.photos.all().order_by(
+                    'order')
+
+                for photo in photos_in_subcategory:
+                    frontend_dataset_counter += 1
+                    photo.order_dataset = frontend_dataset_counter
+
+                photos_by_subcategory[subcategory] = photos_in_subcategory
+
+            return photos_by_subcategory
         return photos
